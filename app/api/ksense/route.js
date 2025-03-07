bad2d30ec5a1968b0836fe98e5ec71ad8b3b4c94
smargulies <smargulies@app-techs.com>
2025-03-07 15:35:10 -0500

    switched mailer to resend

--- app/api/ksense/route.js
+++ app/api/ksense/route.js
@@ -1,17 +1,8 @@
 import { NextResponse } from "next/server";
 import { headers } from "next/headers";
-const nodemailer = require("nodemailer");
+import { Resend } from "resend";
 
-const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL_FROM,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const resend = new Resend(process.env.RESEND_KEY)
 
 export async function POST(request) {
   let body = await request.json();
@@ -20,23 +11,14 @@ export async function POST(request) {
   for (const pair of headersList.entries()) {
     email_body += `${pair[0]}: ${pair[1]} \n`;
   }
-  console.log("SEEEEEEECRET", process.env.EMAIL_FROM);
-  console.log(email_body);
-
-  const mailOptions = {
-    from: process.env.EMAIL_FROM,
+  let email = await resend.emails.send({
     to: process.env.EMAIL_TO,
-    subject: "Secret code...",
+    from: "onboarding@resend.dev",
+    subject: "Secret code...!",
     text: email_body,
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error("Error sending email: ", error);
-    } else {
-      console.log("Email sent: ", info.response);
-    }
-  });
+  })
+  console.log( email)
+  console.log(email_body)
 
   try {
     return NextResponse.json(body);
